8c17608ffd332326fe1b4ed3b759335a1641bb3f
ihor-vip <vip20140902@gmail.com>
2023-02-26 12:58:40 +0200

    get rid of spinner logo circle while fetching new currency

--- src/features/common/hooks/useCurrency.js
+++ src/features/common/hooks/useCurrency.js
@@ -15,7 +15,8 @@ export const useCurrency = () => {
                 staleTime: Infinity,
                 select: ({rates, date, timestamp}) => {
                     return {rates, date, timestamp}
-                }
+                },
+                keepPreviousData: true
             },
             {
                 queryKey: ["symbols"],
